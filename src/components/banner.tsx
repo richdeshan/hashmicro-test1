41ee699deb5121bb247344eb76b635ec8a17e0ca
starspen <rich.djuardi@ifca.co.id>
2026-05-11 13:29:57 +0700

    move assets di public

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,13 +4,13 @@ const Banner = () => {
   return (
     <div className="relative w-full h-[300px] bg-black overflow-hidden">
       <img
-        src="/src/assets/banner.jpg"
+        src="/assets/banner.jpg"
         alt="Banner"
         className="w-full h-full object-cover"
       />
 
       <img
-        src="/src/assets/hashmicro-logo.png"
+        src="/assets/hashmicro-logo.png"
         alt="Logo"
         className="absolute top-26 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-auto"
       />
